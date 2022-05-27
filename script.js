0ab247a616bcb83ca6e8406c59c91698cb600e0b
Luiz Nörnberg <nornberg@gmail.com>
2022-05-27 14:16:49 +0000

    Base do putpixel funcionando.

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-const WIDTH = 100;
-const HEIGHT = 100;
+const WIDTH = 129;
+const HEIGHT = 129;
 
 function draw() {
   const canvas = document.getElementById('canvas');
   canvas.width = WIDTH;
   canvas.height = HEIGHT;
+  //canvas.style.height = "100%";
+  canvas.style.imageRendering = "pixelated";
   if (canvas.getContext) {
     const ctx = canvas.getContext('2d', { alpha: false, antialias: false, depth: false });
     ctx.imageSmoothingEnabled = false;
+    
     const imgBuffer = ctx.createImageData(WIDTH, HEIGHT);
-
     for (let y=0; y<HEIGHT; y++) {
       for (let x=0; x<WIDTH; x++) {
         const idx = 4 * (y * WIDTH + x);
-        imgBuffer.data[idx+0] = 250;
-        imgBuffer.data[idx+1] = 250;
-        imgBuffer.data[idx+2] = 250;
-        imgBuffer.data[idx+3] = 250;
+        imgBuffer.data[idx+0] = 150;
+        imgBuffer.data[idx+1] = 150;
+        imgBuffer.data[idx+2] = 150;
       }
     }
-    
+    ctx.putImageData(imgBuffer, 0, 0);
+    /*
+    ctx.textBaseline = "top";
+    ctx.font = "50px Roboto";
+    ctx.fillStyle = "blue";
+    ctx.fillText("Teste", 10, 10);
+    */
   }
 }
 
