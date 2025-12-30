8093071018e37f9232b1f7209f5c1dc115e1f0f8
Luiz Nörnberg <nornberg@gmail.com>
2025-12-30 04:50:42 +0000

    Contador de frames

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-const WIDTH = 1000;
-const HEIGHT = 1000;
+const WIDTH = 640;
+const HEIGHT = 480;
 let fps = 0;
 let frame_count = 0;
-let frame_time = Date.now();
+let frame_time = 0;
+let last_update_time = 0;
 
 let palette = [];
-let buffer = [][];
+let buffer = [];
 
-const frame = () => {
-  frame_count++;
-  if (Date.now() - frame_time > 1000){
+const frame = (timestamp) => {
+  if (timestamp - frame_time >= 1000){
     fps = frame_count;
-    frame_time = Date.now();
+    frame_time = timestamp;
     frame_count = 0;
   }
-  ctx.putImageData(imgBuffer, 0, 0);
-  ctx.textBaseline = "top";
-  ctx.font = "50px Roboto";
-  ctx.fillStyle = "red";
-  ctx.fillText("TESTE " + (Math.random()*1000).toFixed(0), 10, 10);
-  ctx.fillText(fps + ' fps', imgBuffer.width-140, 10);
-  draw(imgBuffer);
+  if (timestamp - last_update_time >= 33){
+    last_update_time = timestamp;
+    frame_count++;
+    ctx.putImageData(imgBuffer, 0, 0);
+    ctx.textBaseline = "top";
+    ctx.font = "20px Roboto";
+    ctx.fillStyle = "red";
+    ctx.fillText("TESTE. " + (Math.random()*1000).toFixed(0), 10, 10);
+    ctx.fillText(fps + ' fps', imgBuffer.width-140, 10);
+    draw(imgBuffer);
+  }
   window.requestAnimationFrame(frame);
 };
 
@@ -28,21 +32,22 @@ const draw = (imgBuffer) => {
   let lumi = Math.random()*50;
   for (let y = 0; y < imgBuffer.height; y++) {
     for (let x = 0; x < imgBuffer.width; x++) {
-      const idx = 4 * (y * imgBuffer.width + x);
+      let bidx = y * imgBuffer.width + x;
+      let idx = 4 * bidx;
       const cx = x * (lumi / imgBuffer.width);
-      imgBuffer.data[idx + 0] = cx;
-      imgBuffer.data[idx + 1] = cx;
-      imgBuffer.data[idx + 2] = cx;
-      // if (x<=0 || x>=imgBuffer.width-1){
-      //   imgBuffer.data[idx + 0] = 255;
-      //   imgBuffer.data[idx + 1] = 0;
-      //   imgBuffer.data[idx + 2] = 0;
-      // }
-      // if (y<=0 || y>=imgBuffer.height-1){
-      //   imgBuffer.data[idx + 0] = 255;
-      //   imgBuffer.data[idx + 1] = 0;
-      //   imgBuffer.data[idx + 2] = 0;
-      // }
+      imgBuffer.data[idx + 0] = palette[buffer[bidx]].r;
+      imgBuffer.data[idx + 1] = palette[buffer[bidx]].g;
+      imgBuffer.data[idx + 2] = palette[buffer[bidx]].b;
+      if (x<=0 || x>=imgBuffer.width-1){
+        imgBuffer.data[idx + 0] = 255;
+        imgBuffer.data[idx + 1] = 0;
+        imgBuffer.data[idx + 2] = 0;
+      }
+      if (y<=0 || y>=imgBuffer.height-1){
+        imgBuffer.data[idx + 0] = 255;
+        imgBuffer.data[idx + 1] = 0;
+        imgBuffer.data[idx + 2] = 0;
+      }
     }
   }
 };
@@ -53,11 +58,13 @@ const setup = (imgBuffer) => {
   }
   for (let y = 0; y < imgBuffer.height; y++) {
     for (let x = 0; x < imgBuffer.width; x++) {
-      const idx = 4 * (y * imgBuffer.width + x);
+      let bidx = y * imgBuffer.width + x;
+      let idx = 4 * bidx;
       imgBuffer.data[idx + 0] = 0;
       imgBuffer.data[idx + 1] = 0;
       imgBuffer.data[idx + 2] = 0;
       imgBuffer.data[idx + 3] = 255;
+      buffer[bidx] = x % 16;
     }
   }
 };
