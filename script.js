ff3456b6f36d545950a3ca82e3cf76932c12fae3
Luiz Nörnberg <nornberg@gmail.com>
2026-01-04 21:39:57 +0000

    WIP

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ function draw(bg, imgBuffer, elapsed_time) {
 };
 
 function onLineStart(lineIdx, elapsed_time, imgBuffer) {
-  sinWave(bgs[0], lineIdx, elapsed_time, imgBuffer);
+  sinWave2(bgs[0], lineIdx, elapsed_time, imgBuffer);
 }
 
 function render(imgBuffer, elapsed_time) {
@@ -129,7 +129,8 @@ function setup(imgBuffer) {
   for (let y = 0; y < bg.h; y++) {
     for (let x = 0; x < bg.w; x++) {
       let bidx = y * bg.w + x;
-      bg.buffer[bidx] = x % 15;
+      bg.buffer[bidx] = Math.trunc(x * y / 10.61) % 15;
+      //bg.buffer[bidx] = Math.trunc(x  / 10.61) % 15;
       if (y == 0 || x == 0) {
         bg.buffer[bidx] = 15;
       } else if (y == bg.h-1 || x == bg.w-1) {
@@ -159,6 +160,19 @@ function main() {
 }
 
 main();
+
+let sin_delta2 = 0;
+function sinWave2 (bg, lineIdx, elapsed_time, imgBuffer) {
+  let perc = lineIdx / imgBuffer.height + sin_delta2; // percentual do circulo entre 0 e 1.
+  let r = perc * 2 * Math.PI; // 2*PI é um circulo completo (360ª).
+  bg.x = 100 + Math.trunc(Math.sin(r) * 50);
+  if (lineIdx == 0) {
+    sin_delta2 += 0.01 / elapsed_time;
+    if (sin_delta2 >= 1) {
+      sin_delta2 = 0;
+    }
+  }
+}
 
 let sin_delta = 0;
 function sinWave (bg, lineIdx, elapsed_time, imgBuffer) {
