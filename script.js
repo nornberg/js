e8b9f268d6fbb26a02112a4a5b6392770bb36a97
Luiz Nörnberg <nornberg@gmail.com>
2026-01-05 17:30:25 +0000

    Mostra todas cores da paleta na tela, mais um tile personalizado.

--- script.js
+++ script.js
@@ -4,18 +4,32 @@ import * as lowlevel from "./lowlevel.js";
 import * as graphics from "./graphics.js";
 
 function setup() {
-  lowlevel.setTile(0, [
+  lowlevel.setTile(250, [
     0,0,0,0,0,0,0,0,
-    0,1,1,1,1,1,1,0,
-    0,1,2,2,2,2,1,0,
-    0,1,2,3,3,2,1,0,
-    0,1,2,3,3,2,1,0,
-    0,1,2,2,2,2,1,0,
-    0,1,1,1,1,1,1,0,
+    0,10,10,10,10,10,10,0,
+    0,10,20,20,20,20,10,0,
+    0,10,20,30,30,20,10,0,
+    0,10,20,30,30,20,10,0,
+    0,10,20,20,20,20,10,0,
+    0,10,10,10,10,10,10,0,
     0,0,0,0,0,0,0,0,
   ]);
-  lowlevel.setBackgroundTile(0, 0, 0);
-};
+  lowlevel.setBackgroundTile(10, 10, 250);
+  let k = 36;
+  for(let i = 0; i < lowlevel.PALETTE_SIZE; i++){
+    lowlevel.setTile(i, [
+      i,i,i,i,i,i,i,i,
+      i,i,i,i,i,i,i,i,
+      i,i,i,i,i,i,i,i,
+      i,i,i,i,i,i,i,i,
+      i,i,i,i,i,i,i,i,
+      i,i,i,i,i,i,i,i,
+      i,i,i,i,i,i,i,i,
+      i,i,i,i,i,i,i,i,
+    ]);
+    lowlevel.setBackgroundTile(i%k, Math.trunc(i/k), i);
+  };
+}
 
 function main() {
   lowlevel.init();
